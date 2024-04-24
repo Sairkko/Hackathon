2b97f3e45cb22008d78b759e3670838ef3307efc
lorada.andre <lorada.andre@omerin.com>
2024-04-24 17:15:04 +0200

    fin de construction du composant d'un vin

--- front/src/components/WineCardComponent.vue
+++ front/src/components/WineCardComponent.vue
@@ -18,19 +18,19 @@ const props = defineProps({
     <template #content>
         <div class="grid grid-cols-4">
 <!--          <div :class="['col-span-1', 'bg-[url(\'' + props.image + '\')]', 'bg-cover']"/>-->
-          <div class="col-span-1 bg-[url('../assets/atelier.png')] bg-cover"/>
+          <div class="rounded-l-lg col-span-1 bg-[url('../assets/atelier.png')] bg-cover"/>
           <div class="col-span-3 text-left p-2" >
             <ul>
               <li class="font-semibold">{{ props.title}} ({{ props.millesime }})</li>
               <li>{{ props.type }}</li>
               <li>{{ props.cepage }}</li>
-              <li class="font-light italic">{{ props.note }}</li>
+              <li class="font-light italic text-grey">"{{ props.note }}"</li>
             </ul>
             <div class="flex justify-between mt-3">
               <p class="font-semibold">En Stock: {{ props.stock }}</p>
-              <div class="flex justify-end mr-3">
-                <a class="bg-[#2A8DA3] text-white px-3 pb-1 pt-2 mx-2 rounded-md">Modifier</a>
-                <a class="bg-[#E04242] text-white px-3 pb-1 pt-2 mx-2 rounded-md">Supprimer</a>
+              <div class="flex justify-end mr-2">
+                <a class="bg-[#2A8DA3] text-white px-3 pb-1 pt-2 mx-1 rounded-md">Modifier</a>
+                <a class="bg-[#E04242] text-white px-3 pb-1 pt-2 mx-1 rounded-md">Supprimer</a>
               </div>
             </div>
           </div>
