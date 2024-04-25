05973b5b19c7956713ee758da789287c5a849ed6
Michael89100 <michaelyaromishyan@gmail.com>
2024-04-25 14:38:22 +0200

    atelier detail

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -44,8 +44,9 @@ const routes: RouteRecordRaw[] = [
     }
   },
   {
-    path: '/detail',
+    path: '/atelier/:id',
     name: 'WorkshopDetailsPage',
+    props: true,
     component: WorkshopDetailsPage,
   },
   {
--- front/src/views/WorkshopDetailsPages.vue
+++ front/src/views/WorkshopDetailsPages.vue
@@ -7,41 +7,60 @@
         <p class="text-sm md:text-lg mt-2">Une expérience unique dédiée à la découverte des vins de la Vallée du Rhône.</p>
       </div>
     </div>
-
-    <section class="px-10 pt-32  bg-black">
-
-  <div class="grid lg:grid-cols-2 items-center justify-items-center gap-5">
-    <div class="order-1 lg:order-1 shadow-2xl ">
-      <img src="@/assets/imageVin.png" >
-    </div>
-    <div class="order-2 lg:order-2  flex flex-col justify-center text-center  ">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div>
-          <p> Cet atelier est conçu spécialement pour ceux qui débutent dans le monde fascinant de la dégustation du vin, offrant une introduction parfaite aux trésors cachés de cette région réputée. La Vallée du Rhône, berceau de vins d'exception, est célèbre pour sa diversité viticole, offrant une gamme de vins rouges et blancs qui séduisent les palais du monde entier. </p>
-          <p> Au cours de cet atelier, vous apprendrez à identifier les caractéristiques uniques des vins du Rhône, à travers leurs arômes, leurs saveurs, et leurs textures. Nous débuterons par les bases de la dégustation de vin, vous guidant à travers les étapes essentielles pour éveiller vos sens. Vous découvrirez comment observer la robe du vin, comment en apprécier les arômes et comment déguster pour en saisir toute la complexité.  </p>
+    <section class="px-10 pt-24 pb-24 bg-black">
+      <div class="grid lg:grid-cols-3 items-center justify-items-center text-left">
+        <div class="shadow-2xl lg:col-span-1">
+          <img src="@/assets/imageVin.png" alt="Image du Vin" class="mx-auto">
         </div>
-        <div>
-          <h2 class="text-xl font-semibold mb-4">Prochaines sessions</h2>
-          <ul class="list-disc pl-5 mb-4">
-            <li>22/08/2025 à 18h00 </li>
-            <li>Quelque part loin de tout, Lyon, France </li>
-          </ul>
-          <ul class="list-disc pl-5 mb-4">
-            <li>22/08/2025 à 18h00</li>
-            <li>Quelque part loin de tout, Lyon, France</li>
-          </ul>
-        </div>
+        
+          <div class="md:flex md:space-x-8 lg:col-span-2">
+            <div class="md:flex-1">
+              <p> Cet atelier est conçu spécialement pour ceux qui débutent dans le monde fascinant de la dégustation du vin, offrant une introduction parfaite aux trésors cachés de cette région réputée.</p>
+              <p> Au cours de cet atelier, vous apprendrez à identifier les caractéristiques uniques des vins du Rhône, à travers leurs arômes, leurs saveurs, et leurs textures.</p>
+            </div>
+            <div class="md:flex-1 border-l-2 border-lite-grey pl-4">
+
+              <h2 class="text-xl font-semibold mb-4">Prochaines sessions</h2>
+              <ul class="list-none pl-5 mb-4">
+                <li class="text-red ">
+                  <font-awesome-icon :icon="['fas', 'calendar-days']" />
+                    <span class="text-dark-red pl-2"> 22/08/2025 à 18h00</span> 
+                  </li>
+                <li class="text-red">
+                  <font-awesome-icon :icon="['fas', 'location-dot']" />
+                  <span class="text-dark-red pl-2"> Quelque part loin de tout, Lyon, France </span>
+                </li>
+                <li class="mt-2 mb-5"><Button label="Participer"  class="bg-red px-2 py-0.5" /></li>
+              </ul>
+              <ul class="list-none pl-5 mb-4">
+                <li class="text-red ">
+                  <font-awesome-icon :icon="['fas', 'calendar-days']" />
+                    <span class="text-dark-red pl-2"> 22/08/2025 à 18h00</span> 
+                  </li>
+                <li class="text-red">
+                  <font-awesome-icon :icon="['fas', 'location-dot']" />
+                  <span class="text-dark-red pl-2"> Quelque part loin de tout, Lyon, France</span>
+                </li>
+              </ul>
+              
+            </div>
+          </div>
       </div>
-    </div>
-</div>
-</section>
+    </section>
+    
 
   </div>
 </template>
 
 <script>
+import Button from 'primevue/button';
+
 export default {
   name: 'WorkshopDetailsPages',
+  components: {
+    Button
+  },
+  props: ['id']
 };
 </script>
 
