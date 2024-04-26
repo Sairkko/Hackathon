7bcd2d28d4e61d0c294910bd77a59d7f837fc2e7
Sairkko <bastien.98@orange.fr>
2024-04-26 16:11:19 +0200

    fix: bug calendar

--- front/src/views/AtelierPage.vue
+++ front/src/views/AtelierPage.vue
@@ -1,28 +1,15 @@
 <script setup lang="ts">
-import { onMounted, ref, reactive, computed } from 'vue';
+import { onMounted, ref, reactive } from 'vue';
 import Atelier from '../models/Atelier';
 import AtelierApi from '@/api/AtelierApi';
 import AtelierCard from '@/components/AtelierCard.vue';
-import Dropdown from "primevue/dropdown";
 
 const isLoading = ref(false);
 const ateliers = ref<Atelier[]>([]);
 const ateliersParThematique = reactive<{ [key: string]: Atelier[] }>({});
-const selectedThematique = ref(null);
 interface DescriptionMap {
   [key: string]: string;
 }
-
-const thematiqueOptions = computed(() => {
-  return [{ label: 'Tous', value: null }, ...Object.keys(ateliersParThematique).map(key => ({ label: key, value: key }))];
-});
-
-const filteredAteliers = computed(() => {
-  if (selectedThematique.value) {
-    return ateliersParThematique[selectedThematique.value];
-  }
-  return ateliers.value; // Retourne tous les ateliers si aucune thématique n'est sélectionnée
-});
 
 const descriptionParThematique: DescriptionMap = {
   'Exploration Terrestre': 'Explorez les secrets des vins régionaux, des blancs subtils aux rouges profonds, en passant par les spécialités uniques.',
@@ -48,11 +35,11 @@ onMounted(async () => {
 
 function organiserAteliersParThematique() {
   ateliers.value.forEach((atelier) => {
-    const thematique = atelier.thematique || 'Autres'; // Assurez-vous que chaque atelier a une propriété 'thematique'
-    if (!ateliersParThematique[thematique]) {
-      ateliersParThematique[thematique] = [];
+    const thematiqueId = atelier.thematique || 'Autres'; // Utilisez un identifiant unique pour chaque thématique
+    if (!ateliersParThematique[thematiqueId]) {
+      ateliersParThematique[thematiqueId] = []; // Créez un tableau vide si la thématique n'existe pas encore
     }
-    ateliersParThematique[thematique].push(atelier);
+    ateliersParThematique[thematiqueId].push(atelier); // Ajoutez l'atelier au tableau correspondant à son identifiant de thématique
   });
 }
 </script>
@@ -63,16 +50,21 @@ function organiserAteliersParThematique() {
       <font-awesome-icon :icon="'wine-bottle'" class="loading-icon"/>
     </div>
     <div v-else>
-    <div class="flex justify-center md:justify-end my-4 mx-20">
-      <Dropdown v-model="selectedThematique" :options="thematiqueOptions" optionLabel="label" optionValue="value" placeholder="Sélectionnez une thématique" class="p-dropdown border text-dark-red" style="width: 300px" />
-    </div>
-    <section v-for="atelier in filteredAteliers" :key="atelier.id" class="px-4 py-4">
-      <h1 class="text-3xl font-bold text-center text-dark-red mb-10">{{ atelier.thematique }}</h1>
-      <p class="my-4 mx-16 text-center font-semibold">
-        {{ descriptionParThematique[atelier.thematique!] }}
-      </p>
-      <AtelierCard :nom="atelier.nom" :description="atelier.description" :thematique="atelier.thematique" :id="atelier.id"/>
-    </section>
+      <template v-for="thematique in Object.keys(ateliersParThematique)" :key="thematique">
+        <div class="atelier-group px-4 py-4">
+          <h1 class="text-3xl font-bold text-center text-dark-red mb-10">{{ thematique }}</h1>
+          <p class="my-4 mx-16 text-center font-semibold">
+            {{ descriptionParThematique[thematique] }}
+          </p>
+          <div class="atelier-details-container">
+            <template v-for="atelier in ateliersParThematique[thematique]" :key="atelier.id">
+              <div class="atelier-details">
+                <AtelierCard :nom="atelier.nom" :description="atelier.description" :thematique="atelier.thematique" :id="atelier.id"/>
+              </div>
+            </template>
+          </div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
