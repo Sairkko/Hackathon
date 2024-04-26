e9b3a396784b10890f0264a68b5c956229a4306e
lorada.andre <lorada.andre@omerin.com>
2024-04-26 15:21:11 +0200

    commentaire 1 lien navbar admin

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -63,11 +63,11 @@ function toggleMenu() {
         <div class="hidden md:flex" style="z-index: 1001;">
           <div v-if="user?.role === 'ROLE_ADMIN'" class="flex items-center space-x-4">
             <div class="relative" v-click-outside="{ open: isAdminMenuOpen, close: toggleAdminMenu }">
-              <div>
-                <button @click="toggleAdminMenu" type="button" class="inline-flex justify-center w-full rounded-md px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                  Administration de contenu
-                </button>
-              </div>
+<!--              <div>-->
+<!--                <button @click="toggleAdminMenu" type="button" class="inline-flex justify-center w-full rounded-md px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none" id="menu-button" aria-expanded="true" aria-haspopup="true">-->
+<!--                  Administration de contenu-->
+<!--                </button>-->
+<!--              </div>-->
 
               <div v-show="isAdminMenuOpen" class="absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                 <div class="py-1" role="none">
@@ -133,7 +133,7 @@ function toggleMenu() {
                   >
                     Gestion des ateliers
                   </router-link>
-                  <router-link :to="{name: 'HomePage'}" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1" id="menu-item-0">Gestion des inscriptions</router-link>
+<!--                  <router-link :to="{name: 'HomePage'}" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1" id="menu-item-0">Gestion des inscriptions</router-link>-->
                   <router-link
                       :class="{'text-red hover:text-red-800': isActiveRoute('inventaire').value, 'text-gray-700 hover:text-gray-800': !isActiveRoute('inventaire').value}"
                       :to="{ name: 'inventaire' }"
@@ -174,9 +174,9 @@ function toggleMenu() {
           <div v-if="user?.role === 'ROLE_ADMIN'" class="mobile-menu flex-col space-y-1 bg-white shadow-md" v-show="isMenuOpen">
             <div class="relative">
               <div>
-                <button @click="toggleAdminMenu" type="button" class="inline-flex justify-center w-full rounded-md px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                  Administration de contenu
-                </button>
+<!--                <button @click="toggleAdminMenu" type="button" class="inline-flex justify-center w-full rounded-md px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none" id="menu-button" aria-expanded="true" aria-haspopup="true">-->
+<!--                  Administration de contenu-->
+<!--                </button>-->
               </div>
 
               <div style="z-index: 1000;" v-show="isAdminMenuOpen" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
